7985953aea7290dc2e601e584249bdce5d5538b8
Abdullah Sadiq <abdullahsadiq0701@gmail.com>
2023-10-13 16:17:24 +0500

    Added some files

---
+++ Courses/Data Structures & Algorithms/Data Structures & Algorithms Lab (CSL-221)/Lecture Notes/Lab No 04/Part by part/Lab No 04 Language Program Files/Lab04/DLL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    class DLL
+    {
+        Node head; // head of list
+    }
+}
