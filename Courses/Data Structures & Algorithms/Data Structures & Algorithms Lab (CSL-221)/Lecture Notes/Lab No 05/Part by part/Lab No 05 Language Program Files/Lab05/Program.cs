4b5bc04fcbe7b8225daba80263d77d12c1701bef
Abdullah Sadiq <abdullahsadiq0701@gmail.com>
2023-10-18 13:40:46 +0500

    Added some files

--- Courses/Data Structures & Algorithms/Data Structures & Algorithms Lab (CSL-221)/Lecture Notes/Lab No 05/Part by part/Lab No 05 Language Program Files/Lab05/Program.cs
+++ Courses/Data Structures & Algorithms/Data Structures & Algorithms Lab (CSL-221)/Lecture Notes/Lab No 05/Part by part/Lab No 05 Language Program Files/Lab05/Program.cs
@@ -8,8 +8,83 @@ namespace Lab05
 {
     class Program
     {
-        static void Main(string[] args)
+            internal class Stack
+            {
+                static readonly int MAX = 1000;
+                int top;
+                int[] stack = new int[MAX];
+                bool isEmpty()
+                {
+                    return (top < 0);
+                }
+                public Stack()
+                {
+                    top = -1;
+                }
+                internal bool Push(int data)
+                {
+                    if (top >= MAX)
+                    {
+                        Console.WriteLine("Stack Overflow");
+                        return false;
+                    }
+                    else
+                    {
+                        stack[++top] = data;
+                        return true;
+                    }
+                }
+                internal int pop()
+                {
+                    if (top < 0)
+                    {
+                        Console.WriteLine("Stack Overflow");
+                        return 0;
+                    }
+                    else
+                    {
+                        int value = stack[top--];
+                        return value;
+                    }
+                }
+                internal void Peek()
+                {
+                    if (top < 0)
+                    {
+                        Console.WriteLine("Stack Overflow");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The top most value is: {0}", stack[top]);
+                    }
+                }
+                internal void printStack()
+                {
+                    if (top < 0)
+                {
+                        Console.WriteLine("Stack Overflow");
+                        return;
+                }
+                    else
+                {
+                        Console.WriteLine("Items in the stack are: ");
+                        for (int i = top; i >= 0; i--)
+                    {
+                            Console.WriteLine(stack[i]);
+                    }
+                }
+            }
+        }
+            public static void Main(string[] args)
         {
+                Stack myStack = new Stack();
+                myStack.Push(10);
+                myStack.Push(20);
+                myStack.Push(30);
+                myStack.Push(40);
+                myStack.printStack();
+                myStack.Peek();
 
         }
     }
