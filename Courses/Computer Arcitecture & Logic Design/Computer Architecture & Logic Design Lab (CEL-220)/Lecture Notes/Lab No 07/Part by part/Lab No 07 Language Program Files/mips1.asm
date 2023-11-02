b180da703d27296a2c802a964ad5c1c2ae97a0e4
Abdullah Sadiq <abdullahsadiq0701@gmail.com>
2023-11-02 20:47:02 +0500

    Did some work

--- Courses/Computer Arcitecture & Logic Design/Computer Architecture & Logic Design Lab (CEL-220)/Lecture Notes/Lab No 07/Part by part/Lab No 07 Language Program Files/mips1.asm
+++ Courses/Computer Arcitecture & Logic Design/Computer Architecture & Logic Design Lab (CEL-220)/Lecture Notes/Lab No 07/Part by part/Lab No 07 Language Program Files/mips1.asm
@@ -1,19 +1,43 @@
 .data
-outPrompt: .asciiz "you are out of loop"
-
+prompt: .asciiz "Enter Value"
+sum_message: .asciiz "The sum of integers from 1 to N is: "
 .text
-li $t0, 0 #i
+main:
+ li $v0, 4
+    la $a0, prompt
+    syscall
+li $v0,5 # 1 INPUT
+syscall
+    # Store the user's input in register $t0.
+    move $t0, $v0
 
-dowhile:
-   beq $t0, 10, out
-   move $a0, $t0
-   li $v0, 1
-   syscall
-   addi $t0, $t0, 1
-   
-out:
-  la $a0, outPrompt
-  li $v0, 4
-  syscall
-  li $v0, 10
-  syscall
+ li $t1, 0
+
+    # Start the loop.
+loop:
+    # Add the current loop iteration to the sum variable.
+    add $t1, $t1, $t0
+
+    # Decrement the loop counter.
+    sub $t0, $t0, 1
+
+    # Check if the loop counter is equal to zero.
+    beq $t0, $zero, end
+
+    # Jump back to the beginning of the loop.
+    j loop
+
+end:
+    # Print the sum message.
+    li $v0, 4
+    la $a0, sum_message
+    syscall
+
+    # Print the sum of integers from 1 to N.
+    move $a0, $t1
+    li $v0, 1
+    syscall
+
+    # Exit the program.
+    li $v0, 10
+    syscall
